d214be3d4b73c283bee9a5c4b3386c8d7bdbf4ea
naya <naya@etourline.com>
2018-11-30 11:13:11 +0900

    데이터베이스 시딩

--- database/factories/ModelFactory.php
+++ database/factories/ModelFactory.php
@@ -13,13 +13,13 @@ use Faker\Generator as Faker;
 |
 */
 
-$factory->define(App\User::class, function (Faker $faker) {
+$factory->define(App\Article::class, function (Faker $faker) {
+    $date = $faker->dateTimeThisMonth;
+
     return [
-        'name' => $faker->name,
-        'email' => $faker->unique()->safeEmail,
-        //'email_verified_at' => now(),
-        //'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
-        'password' => bcrypt('password'),
-        'remember_token' => str_random(10),
+        'title' => $faker->sentence(),
+        'content' => $faker->paragraph(),
+        'created_at' => $date,
+        'updated_at' => $date,
     ];
 });
